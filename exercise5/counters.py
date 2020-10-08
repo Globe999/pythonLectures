85471408e33f273e56fa7e06974a483de9fcece4
Globee <marten.sanderod@gmail.com>
2020-10-08 14:48:48 +0200

    Finished exercise5

--- exercise5/counters.py
+++ exercise5/counters.py
@@ -1,7 +1,8 @@
 class SimpleCounter():
-    def __init__(self):
+    def __init__(self, value = 0):
         """Creates a Simple Counter"""
-        self.value = 0
+        self.value = value
+            
     def count(self):
         self.value += 1
 
@@ -11,51 +12,27 @@ class SimpleCounter():
     def getValue(self):
         return self.value
 
-class BoundedCounter():
+class BoundedCounter(SimpleCounter):
     """
     Creates a Bounded Counter with SimpleCounter as parent class
-    Fråga till Niklas: Kan man skapa en parent-klass som hanterar alla count, reset osv men om man behöver, kan man overrida dem genom att skriva metoden igen?
-
     """
     def __init__(self, init, modulus):
-        self.value = init
+        super().__init__(init)
         self.modulus = modulus
 
     def count(self):
-        #Doesn't work, needs fixing
-        # print(self.value % self.modulus)
+        self.value = (self.value + 1) % self.modulus
 
-        self.value = 0 if self.value == self.modulus-1 else self.value +1
-
-    def reset(self):
-        self.value = 0
-
-    def getValue(self):
-        return self.value
-
-class ChainedCounter():
+class ChainedCounter(BoundedCounter):
     """
     Creates a Chained Counter
     """
     def __init__(self, init, modulus, next):
-        self.value = init
-        self.modulus = modulus
+        super().__init__(init,modulus)
         self.next = next
 
     def count(self):
-        #Doesn't work, needs fixing
-        # print(self.value % self.modulus)
-
-        if self.value == self.modulus-1:
-            self.reset()
-            if self.next: self.next.count()
-        else:
-            self.value += 1
-
-    def reset(self):
-        self.value = 0
-
-    def getValue(self):
-        return self.value
+        super().count()
+        if self.value == 0: self.next.count()
 
     
